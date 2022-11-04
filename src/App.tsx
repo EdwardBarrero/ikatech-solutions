5d3c01cf1aa148c8cc1fc535878fbf9e9c3b6ef0
Edward Barrero <edwbarrero@gmail.com>
2022-11-04 15:15:23 -0500

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div>
       <Routes>
-        <Route path='/producto' element={<FeatureProductDetail />} />
+        <Route path='/' element={<FeatureProductDetail />} />
       </Routes>
     </div>
   );
