197ed948bec0f99c77d8644583d52bcd89d57446
Edward Barrero <edwbarrero@gmail.com>
2022-11-04 13:48:00 -0500

    icon updated and feature counter deleted

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
 import ProductSlice from './product/domain/services/ProductSlice';
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
     products : ProductSlice,
   },
 });
